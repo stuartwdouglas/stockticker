c0678af662b751c81f1b1efc077a6b90f848f07d
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-01-20 11:38:53 +1100

    rmeove queries

--- pricedb/db/queries/queries.sql
+++
@@ -1,5 +0,0 @@
--- name: StorePrice :exec
-INSERT INTO prices (code, price, timestamp, currency) VALUES (?, ?, ?, ?);
-
--- name: LoadPrices :many
-SELECT code, price, timestamp, currency FROM prices ORDER BY timestamp DESC;
